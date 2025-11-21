22b608cf1d0f85c544d90dfab97591dba9f21532
Pablo Risi <pablomrisi@hotmail.com>
2025-11-21 19:54:21 -0300

    docker

--- Server/index.js
+++ Server/index.js
@@ -51,7 +51,7 @@ app.post('/send-email', async (req, res) => {
         console.log('📧 Enviando email con Resend...');
 
         const { data, error } = await resend.emails.send({
-            from: 'Portfolio Contact <onboarding@resend.dev>',
+              from: 'Portfolio Contact <onboarding@resend.dev>',
             to: process.env.EMAIL_USER,
             replyTo: email,
             subject: `Nuevo contacto: ${name} - Portafolio`,
